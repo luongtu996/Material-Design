26b33a44c84c76d9371028daa7e8b476d74cc65f
Ridho Prasetya <ndiing@users.noreply.github.com>
2019-03-28 00:39:38 +0700

    fab

---
+++ fab/fab.js
@@ -0,0 +1,60 @@
+import MDRipple from '../ripple/ripple.js';
+
+class MDFab extends HTMLElement {
+
+    template = `
+        <style>
+        @import '../icon/icon.css';
+        @import './fab.css';
+        </style>
+        ${this.icon ? `<span class="fab__icon icon">${this.icon}</span>` : ''} 
+        ${this.text ? `<span class="fab__text">${this.text}</span>` : ''}
+    `;
+
+    constructor() {
+        super();
+
+        this.attachShadow({
+            mode: 'open'
+        }).innerHTML = this.template;
+
+        let ripple = new MDRipple(this);
+    }
+
+    get icon() {
+        return this.getAttribute('icon');
+    }
+
+    get text() {
+        return this.getAttribute('text');
+    }
+
+    set icon(value) {
+        return this.setAttribute('icon', value);
+    }
+
+    set text(value) {
+        return this.setAttribute('text', value);
+    }
+
+    connectedCallback() {
+        // updateStyle(this);
+    }
+
+    disconnectedCallback() { }
+
+    adoptedCallback() { }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        // updateStyle(this);
+    }
+
+    static get observedAttributes() {
+        return [
+            'icon',
+            'text'
+        ];
+    }
+}
+
+customElements.define('md-fab', MDFab);
--- material.js
+++ material.js
@@ -53,3 +53,59 @@ document.querySelectorAll('.checkbox').forEach(el => {
         inputEl.indeterminate = inputEl.hasAttribute('indeterminate');
     }
 })
+
+let att = [];
+document.querySelectorAll(`
+    md-fab,
+    md-button
+`).forEach(el => {
+    for (var i = el.attributes.length - 1; i >= 0; i--) {
+        att.push(el.attributes[i].name);
+    }
+});
+att = att.filter((v, i, a) => a.indexOf(v) === i);
+// att = att.sort((a, b) => a.localeCompare(b))
+document.querySelector('body').insertAdjacentHTML('beforeend',
+    `
+<style>
+.demo-table th {
+    height: 48px;
+    padding: 0 16px;
+    border-top: 1px solid rgba(0, 0, 0, 0.12);
+    border-right: 1px solid rgba(0, 0, 0, 0.12);
+    border-bottom: 2px solid rgba(0, 0, 0, 0.12);
+}
+.demo-table td {
+    height: 48px;
+    padding: 0 16px;
+    border-right: 1px solid rgba(0, 0, 0, 0.12);
+    border-bottom: 1px solid rgba(0, 0, 0, 0.12);
+}
+.demo-table th:first-child {
+    border-left: 1px solid rgba(0, 0, 0, 0.12);
+}
+.demo-table td:first-child {
+    border-left: 1px solid rgba(0, 0, 0, 0.12);
+}
+.demo-code {
+}
+</style>
+<br>
+<br>
+<table class="demo-table">
+    <thead>
+        <tr>
+            <th>Prop Name</th>
+            <th>Type</th>
+            <th>Description</th>
+        </tr>
+    </thead>
+    <tbody>
+        ${att.map(c =>
+        `<tr>
+            <td><code class="demo-code">${c}</code></td>
+            <td></td>
+            <td></td>
+        </tr>`).join('')}
+    </tbody>
+</table>`);
