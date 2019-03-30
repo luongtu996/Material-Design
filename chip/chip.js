5ff63422f962aadfb10321105cb23707a5f518dc
Ridho Prasetya <ndiing@users.noreply.github.com>
2019-03-30 22:58:22 +0700

    md-chip

--- chip/chip.js
+++ chip/chip.js
@@ -29,7 +29,7 @@ class MDChip extends HTMLElement {
     }
 
     handleClick(event) {
-        this.classList.toggle('is-activated');
+        // this.classList.toggle('is-activated');
     }
 
     chipActionHandleClick(event) {
