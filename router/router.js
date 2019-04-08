a1601865c3082b3e9508e8e684ea82d979dff96f
Ridho Prasetya <ndiing@users.noreply.github.com>
2019-04-08 09:08:24 +0700

    only load changes hash

--- router/router.js
+++ router/router.js
@@ -18,6 +18,11 @@ customElements.define('md-router', RouterComponent);
 */
 export default class Router {
 
+    // user by system
+    // to compare with new alias
+    aliasing = '';
+
+    // base route template
     route = {
         path: undefined,
         component: undefined,
@@ -27,34 +32,43 @@ export default class Router {
     }
 
     constructor(routes = []) {
+        // intialize route
         this.routes = routes;
+        // binding `this` to listener
         this.listenHashchange = this.listenHashchange.bind(this);
 
+        // listener
         window.addEventListener('DOMContentLoaded', this.listenHashchange);
         window.addEventListener('hashchange', this.listenHashchange);
     }
 
+    // routes map
     set routes(routes = []) {
         this._routes = this.routesMap(routes);
     }
 
+    // current routes map
     get routes() {
         return this._routes;
     }
 
+    // set hash
     set hash(path) {
         window.location.hash = path;
     }
 
+    // get current hash
     get hash() {
         return window.location.hash.replace(/#/, '').replace(/\?.*/, '');
     }
 
+    // change hash to path root
     get alias() {
         let matches = this.routeMatchName(this.hash);
         return matches ? matches.pathAlias : matches;
     }
 
+    // handle child route
     get aliasMap() {
         return this.alias.split('/').map((value, index, array) => {
             let path = array.slice(0, index);
@@ -63,6 +77,7 @@ export default class Router {
         });
     }
 
+    // getting hash with query params
     get queryParams() {
         let query = window.location.hash.match(/\?.*(?:$)/);
 
@@ -75,28 +90,39 @@ export default class Router {
 
         return {};
     }
-
+    
+    // render...
     listenHashchange(event) {
-        this.aliasMap.map(alias => {
+        this.aliasMap.map((alias, index, array) => {
             let matches = this.routeMatchAlias(alias);
-            let routerComponent = document.querySelectorAll('md-router');
-            let routerComponentNextElementSibling = routerComponent[matches.pathId].nextElementSibling
+            let aliasingList = this.aliasing.split('/');
+            let aliasList = alias.split('/');
+            
+            if (aliasingList[index] !== aliasList[index] || (array.length - 1) == index) {
 
-            if (matches) {
-
-                if (routerComponentNextElementSibling) {
-                    routerComponentNextElementSibling.remove();
+                if (matches) {                    
+                    let routerComponent = document.querySelectorAll('md-router');
+                    let routerComponentNextElementSibling = routerComponent[matches.pathId].nextElementSibling
+                    
+                    if (routerComponentNextElementSibling) {
+                        routerComponentNextElementSibling.remove();
+                    }
+                    
+                    routerComponent[matches.pathId].insertAdjacentHTML('afterend', matches.component);
                 }
 
-                routerComponent[matches.pathId].insertAdjacentHTML('afterend', matches.component);
+                this.aliasing = alias;
             }
         });
     }
 
+    // regexp for pathName & path alias
     pathToRegExp(path) {
         return new RegExp(path.replace(/:\w+/g, '([^\/]+)').replace(/\*/g, '(?:.*)') + '(?:$)');
     }
 
+    // maching hash with path
+    // maching alias with path
     hashMatch(hash, path) {
         let matches = hash.match(this.pathToRegExp(path));
 
@@ -107,10 +133,11 @@ export default class Router {
         return null;
     }
 
+    // flatten routes
     routesMap(routesTree, pathName = '', pathAlias = '', pathId = 0) {
         return routesTree.reduce((routes, route, index) => {
             route = Object.assign({}, this.route, route);
-            index = route.path.match(/:\w+|\*/g) ? route.path : index;
+            index = route.path.match(/:\w+/g) ? route.path : index;
 
             route.pathName = `${pathName}/${route.path}`.slice(1);
             route.pathAlias = `${pathAlias}/${index}`.slice(1);
@@ -126,10 +153,12 @@ export default class Router {
         }, []);
     }
 
+    // matches by pathName
     routeMatchName(hash) {
         return this.routes.find(route => this.hashMatch(hash, route.pathName));
     }
-
+    
+    // matches by pathAlias
     routeMatchAlias(hash) {
         return this.routes.find(route => this.hashMatch(hash, route.pathAlias));
     }
