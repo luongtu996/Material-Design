f4aad8bdb09ff9fa7bf806c0ae3891c17731663a
Ridho Prasetya <ndiing@users.noreply.github.com>
2019-04-14 04:50:21 +0700

    test

--- com/home/home.model.js
+++ com/home/home.model.js
@@ -1,7 +1,16 @@
 export default class HomeModel {
     // data = []
+
+    // api = {}
     
     constructor() {
         this.data = []
+
+        this.api = {
+            create: '',
+            read: '',
+            update: '',
+            remove: '',
+        }
     }
 }
