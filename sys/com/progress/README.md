5869b714d9f620e7b1151e7310806fad52e38273
Ridho Prasetya <ndiing@users.noreply.github.com>
2019-04-15 10:19:10 +0700

    progress

---
+++ sys/com/progress/README.md
@@ -0,0 +1,24 @@
+# Progress
+
+### Tag
+Tag Name | Description
+--- | --- 
+wk-progress | 
+
+### Prop
+Prop Name | Type | Description
+--- | --- | ---
+value | String |
+indeterminate | Boolean |
+circular | Boolean |
+
+### Event
+Event Name | Params | Description
+--- | --- | ---
+ |  | 
+
+### CSS
+CSS Class | Description
+--- | --- 
+.progress |
+.progress--circular |
--- sys/com/progress/progress.js
+++ sys/com/progress/progress.js
@@ -1,25 +1,61 @@
-document.querySelectorAll('.progress').forEach(progress => {
-    let leftAngle = '';
-    let leftColor = '';
-    let rightAngle = '';
-    let rightColor = '';
+class WKProgress extends HTMLElement {
 
-    if (progress.value <= 50) {
-        leftAngle = `90deg`;
-        leftColor = `#f5f5f5`;
-        rightAngle = `${90 + 3.6 * progress.value}deg`;
-        rightColor = `#6200ee`;
-    }
-    else {
-        leftAngle = `${-90 + 3.6 * (progress.value - 50)}deg`;
-        leftColor = `#6200ee`;
-        rightAngle = `270deg`;
-        rightColor = `#6200ee`;
+    constructor() {
+        super()
+        this.template()
     }
 
-    progress.style.setProperty('--progress-before-width', `${progress.value}%`);
-    progress.style.setProperty('--progress-circular-before-left-angle', leftAngle);
-    progress.style.setProperty('--progress-circular-before-left-color', leftColor);
-    progress.style.setProperty('--progress-circular-before-right-angle', rightAngle);
-    progress.style.setProperty('--progress-circular-before-right-color', rightColor);
-});
+    template() {
+        this.innerHTML = `
+            ${this.circular ? `<svg viewBox="0 0 40 40">
+                <circle cx="20" cy="20" r="18" />
+            </svg>`:``}
+        `
+
+        this.style.setProperty('--progress-value', this.value);
+    }
+
+    connectedCallback() { }
+
+    disconnectedCallback() { }
+
+    adoptedCallback() { }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        this.template()
+    }
+
+    static get observedAttributes() {
+        return [
+            'value',
+            'indeterminate',
+            'circular',
+        ]
+    }
+
+    get value() {
+        return this.getAttribute('value')
+    }
+
+    set value(value) {
+        this.setAttribute('value', value)
+    }
+
+    get indeterminate() {
+        return this.hasAttribute('indeterminate')
+    }
+
+    set indeterminate(value) {
+        this.setAttribute('indeterminate', '')
+    }
+
+    get circular() {
+        return this.hasAttribute('circular')
+    }
+
+    set circular(value) {
+        this.setAttribute('circular', '')
+    }
+}
+
+customElements.define('wk-progress', WKProgress)
