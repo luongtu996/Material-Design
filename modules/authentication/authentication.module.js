15bb3e7024463afbb2177785c8a2663517e4b9ad
Ridho Prasetya <ndiing@users.noreply.github.com>
2019-04-17 14:39:31 +0700

    test

--- modules/authentication/authentication.module.js
+++ modules/authentication/authentication.module.js
@@ -1,1 +1,16 @@
 // Authentication Layout
+class AuthenticationModule extends HTMLElement {
+
+    constructor() {
+        super()
+
+        this.innerHTML = `
+            <h1>AuthenticationModule</h1>
+            <wk-router></wk-router>
+        `
+    }
+}
+
+customElements.define('authentication-module', AuthenticationModule)
+
+export default `<authentication-module></authentication-module>`
