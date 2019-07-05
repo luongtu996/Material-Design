cd13bb8c7589b9c5f5ac1c43e67c48dc7eed05b3
Ridho Prasetya <ndiing@users.noreply.github.com>
2019-07-06 01:03:08 +0700

    wk-button

--- app/button/button.js
+++ app/button/button.js
@@ -2,79 +2,79 @@ class ButtonComponent extends HTMLElement {
 
     connectedCallback() {
         this.innerHTML = `
-            <div class="layout">
-                <div class="layout__grid">
+            <wk-layout>
+                <wk-layout-grid>
 
-                    <div class="layout__column layout__column--3-large layout__column--4-medium layout__column--2-small">
+                    <wk-layout-column columnLarge="3">
                         <wk-button>text</wk-button>
-                    </div>
-                    <div class="layout__column layout__column--3-large layout__column--4-medium layout__column--2-small">
+                    </wk-layout-column>
+                    <wk-layout-column columnLarge="3">
                         <wk-button outlined>text</wk-button>
-                    </div>
-                    <div class="layout__column layout__column--3-large layout__column--4-medium layout__column--2-small">
+                    </wk-layout-column>
+                    <wk-layout-column columnLarge="3">
                         <wk-button contained>text</wk-button>
-                    </div>
-                    <div class="layout__column layout__column--3-large layout__column--4-medium layout__column--2-small">
+                    </wk-layout-column>
+                    <wk-layout-column columnLarge="3">
                         <wk-button-group>
                             <wk-button toggle icon="&#xE721;"></wk-button>
                             <wk-button toggle icon="&#xE722;"></wk-button>
                             <wk-button toggle icon="&#xE723;"></wk-button>
                         </wk-button-group>
-                    </div>
+                    </wk-layout-column>
 
-                    <div class="layout__column layout__column--3-large layout__column--4-medium layout__column--2-small">
+                    <wk-layout-column columnLarge="3">
                         <wk-button icon="&#xE721;">text</wk-button>
-                    </div>
-                    <div class="layout__column layout__column--3-large layout__column--4-medium layout__column--2-small">
+                    </wk-layout-column>
+                    <wk-layout-column columnLarge="3">
                         <wk-button icon="&#xE721;" outlined>text</wk-button>
-                    </div>
-                    <div class="layout__column layout__column--3-large layout__column--4-medium layout__column--2-small">
+                    </wk-layout-column>
+                    <wk-layout-column columnLarge="3">
                         <wk-button icon="&#xE721;" contained>text</wk-button>
-                    </div>
-                    <div class="layout__column layout__column--3-large layout__column--4-medium layout__column--2-small">
+                    </wk-layout-column>
+                    <wk-layout-column columnLarge="3">
                         <wk-button-group multiple>
                             <wk-button toggle icon="&#xE721;"></wk-button>
                             <wk-button toggle icon="&#xE722;"></wk-button>
                             <wk-button toggle icon="&#xE723;"></wk-button>
                         </wk-button-group>
-                    </div>
+                    </wk-layout-column>
 
-                    <div class="layout__column layout__column--3-large layout__column--4-medium layout__column--2-small">
+                    <wk-layout-column columnLarge="3">
                         <wk-button disabled>text</wk-button>
-                    </div>
-                    <div class="layout__column layout__column--3-large layout__column--4-medium layout__column--2-small">
+                    </wk-layout-column>
+                    <wk-layout-column columnLarge="3">
                         <wk-button disabled outlined>text</wk-button>
-                    </div>
-                    <div class="layout__column layout__column--3-large layout__column--4-medium layout__column--2-small">
+                    </wk-layout-column>
+                    <wk-layout-column columnLarge="3">
                         <wk-button disabled contained>text</wk-button>
-                    </div>
-                    <div class="layout__column layout__column--3-large layout__column--4-medium layout__column--2-small">
+                    </wk-layout-column>
+                    <wk-layout-column columnLarge="3">
                         <wk-button-group>
                             <wk-button disabled toggle icon="&#xE721;"></wk-button>
                             <wk-button disabled toggle icon="&#xE722;"></wk-button>
                             <wk-button disabled toggle icon="&#xE723;"></wk-button>
                         </wk-button-group>
-                    </div>
+                    </wk-layout-column>
 
-                    <div class="layout__column layout__column--3-large layout__column--4-medium layout__column--2-small">
+                    <wk-layout-column columnLarge="3">
                         <wk-button disabled icon="&#xE721;">text</wk-button>
-                    </div>
-                    <div class="layout__column layout__column--3-large layout__column--4-medium layout__column--2-small">
+                    </wk-layout-column>
+                    <wk-layout-column columnLarge="3">
                         <wk-button disabled icon="&#xE721;" outlined>text</wk-button>
-                    </div>
-                    <div class="layout__column layout__column--3-large layout__column--4-medium layout__column--2-small">
+                    </wk-layout-column>
+                    <wk-layout-column columnLarge="3">
                         <wk-button disabled icon="&#xE721;" contained>text</wk-button>
-                    </div>
-                    <div class="layout__column layout__column--3-large layout__column--4-medium layout__column--2-small">
+                    </wk-layout-column>
+                    <wk-layout-column columnLarge="3">
                         <wk-button-group multiple>
                             <wk-button disabled toggle icon="&#xE721;"></wk-button>
                             <wk-button disabled toggle icon="&#xE722;"></wk-button>
                             <wk-button disabled toggle icon="&#xE723;"></wk-button>
                         </wk-button-group>
-                    </div>
+                    </wk-layout-column>
 
-                </div>
-            </div>
+                </wk-layout-grid>
+            </wk-layout>
         `
     }
 }
