5942283a9946b99945be689f9b482739c5c22bd4
Ridho Prasetya <ndiing@users.noreply.github.com>
2019-07-05 19:58:28 +0700

    removed wk-checkbox login test

--- components/checkbox/index.1.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll(".checkbox").forEach(element => {
-    let checkboxInput = element.querySelector(".checkbox__input")
-
-    if (checkboxInput.hasAttribute("indeterminate")) {
-        checkboxInput.indeterminate = true
-    }
-})
