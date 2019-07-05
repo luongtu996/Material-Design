27c7d56d4917d3516d7384823bf8452234c65062
Ridho Prasetya <ndiing@users.noreply.github.com>
2019-07-06 01:31:36 +0700

    wkTypography

--- app/typography/typography.js
+++ app/typography/typography.js
@@ -2,29 +2,27 @@ class TypographyComponent extends HTMLElement {
 
     connectedCallback() {
         this.innerHTML = `
-            <div class="layout">
-                <div class="layout__grid">
-                    <div class="layout__column layout__column--12-large layout__column--8-medium layout__column--4-small">
-                    
-                        <h1 class="typography typography--h1">H1</h1>
-                        <h2 class="typography typography--h2">H2</h2>
-                        <h3 class="typography typography--h3">H3</h3>
-                        <h4 class="typography typography--h4">H4</h4>
-                        <h5 class="typography typography--h5">H5</h5>
-                        <h6 class="typography typography--h6">H6</h6>
-                        <h1 class="typography typography--subtitle1">Subtitle1</h1>
-                        <h2 class="typography typography--subtitle2">Subtitle2</h2>
-                        <p class="typography typography--body1">Body1</p>
-                        <p class="typography typography--body2">Body2</p>
-                        <p class="typography typography--button">BUTTON</p>
-                        <p class="typography typography--caption">Caption</p>
-                        <p class="typography typography--overline">OVERLINE</p>
+            <wk-layout>
+                <wk-layout-grid>
+                    <wk-layout-column columnLarge="12" columnMedium="8" columnSmall="4">
 
-                    </div>
-                    <div class="layout__column layout__column--12-large layout__column--8-medium layout__column--4-small"></div>
-                    <div class="layout__column layout__column--12-large layout__column--8-medium layout__column--4-small"></div>
-                </div>
-            </div>
+                        <h1 wkTypography="h1">H1</h1>
+                        <h2 wkTypography="h2">H2</h2>
+                        <h3 wkTypography="h3">H3</h3>
+                        <h4 wkTypography="h4">H4</h4>
+                        <h5 wkTypography="h5">H5</h5>
+                        <h6 wkTypography="h6">H6</h6>
+                        <h1 wkTypography="subtitle1">Subtitle1</h1>
+                        <h2 wkTypography="subtitle2">Subtitle2</h2>
+                        <p wkTypography="body1">Body1</p>
+                        <p wkTypography="body2">Body2</p>
+                        <p wkTypography="button">BUTTON</p>
+                        <p wkTypography="caption">Caption</p>
+                        <p wkTypography="overline">OVERLINE</p>
+
+                    </wk-layout-column>
+                </wk-layout-grid>
+            </wk-layout>
         `
     }
 }
