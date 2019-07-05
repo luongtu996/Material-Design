015665455337a8112038f06a5870c5f1acbeb200
Ridho Prasetya <ndiing@users.noreply.github.com>
2019-07-06 01:08:53 +0700

    wkElevation

--- app/elevation/elevation.js
+++ app/elevation/elevation.js
@@ -2,35 +2,35 @@ class ElevationComponent extends HTMLElement {
 
     connectedCallback() {
         this.innerHTML = `
-            <div class="layout">
-                <div class="layout__grid">
-                    <div class="layout__column layout__column--2 elevation--0" style="height: calc(64px * 3);"></div>
-                    <div class="layout__column layout__column--2 elevation--1" style="height: calc(64px * 3);"></div>
-                    <div class="layout__column layout__column--2 elevation--2" style="height: calc(64px * 3);"></div>
-                    <div class="layout__column layout__column--2 elevation--3" style="height: calc(64px * 3);"></div>
-                    <div class="layout__column layout__column--2 elevation--4" style="height: calc(64px * 3);"></div>
-                    <div class="layout__column layout__column--2 elevation--5" style="height: calc(64px * 3);"></div>
-                    <div class="layout__column layout__column--2 elevation--6" style="height: calc(64px * 3);"></div>
-                    <div class="layout__column layout__column--2 elevation--7" style="height: calc(64px * 3);"></div>
-                    <div class="layout__column layout__column--2 elevation--8" style="height: calc(64px * 3);"></div>
-                    <div class="layout__column layout__column--2 elevation--9" style="height: calc(64px * 3);"></div>
-                    <div class="layout__column layout__column--2 elevation--10" style="height: calc(64px * 3);"></div>
-                    <div class="layout__column layout__column--2 elevation--11" style="height: calc(64px * 3);"></div>
-                    <div class="layout__column layout__column--2 elevation--12" style="height: calc(64px * 3);"></div>
-                    <div class="layout__column layout__column--2 elevation--13" style="height: calc(64px * 3);"></div>
-                    <div class="layout__column layout__column--2 elevation--14" style="height: calc(64px * 3);"></div>
-                    <div class="layout__column layout__column--2 elevation--15" style="height: calc(64px * 3);"></div>
-                    <div class="layout__column layout__column--2 elevation--16" style="height: calc(64px * 3);"></div>
-                    <div class="layout__column layout__column--2 elevation--17" style="height: calc(64px * 3);"></div>
-                    <div class="layout__column layout__column--2 elevation--18" style="height: calc(64px * 3);"></div>
-                    <div class="layout__column layout__column--2 elevation--19" style="height: calc(64px * 3);"></div>
-                    <div class="layout__column layout__column--2 elevation--20" style="height: calc(64px * 3);"></div>
-                    <div class="layout__column layout__column--2 elevation--21" style="height: calc(64px * 3);"></div>
-                    <div class="layout__column layout__column--2 elevation--22" style="height: calc(64px * 3);"></div>
-                    <div class="layout__column layout__column--2 elevation--23" style="height: calc(64px * 3);"></div>
-                    <div class="layout__column layout__column--2 elevation--24" style="height: calc(64px * 3);"></div>
-                </div>
-            </div>
+            <wk-layout>
+                <wk-layout-grid>
+                    <wk-layout-column column="2" wkElevation="0" style="height: calc(64px * 3);"></wk-layout-column>
+                    <wk-layout-column column="2" wkElevation="1" style="height: calc(64px * 3);"></wk-layout-column>
+                    <wk-layout-column column="2" wkElevation="2" style="height: calc(64px * 3);"></wk-layout-column>
+                    <wk-layout-column column="2" wkElevation="3" style="height: calc(64px * 3);"></wk-layout-column>
+                    <wk-layout-column column="2" wkElevation="4" style="height: calc(64px * 3);"></wk-layout-column>
+                    <wk-layout-column column="2" wkElevation="5" style="height: calc(64px * 3);"></wk-layout-column>
+                    <wk-layout-column column="2" wkElevation="6" style="height: calc(64px * 3);"></wk-layout-column>
+                    <wk-layout-column column="2" wkElevation="7" style="height: calc(64px * 3);"></wk-layout-column>
+                    <wk-layout-column column="2" wkElevation="8" style="height: calc(64px * 3);"></wk-layout-column>
+                    <wk-layout-column column="2" wkElevation="9" style="height: calc(64px * 3);"></wk-layout-column>
+                    <wk-layout-column column="2" wkElevation="10" style="height: calc(64px * 3);"></wk-layout-column>
+                    <wk-layout-column column="2" wkElevation="11" style="height: calc(64px * 3);"></wk-layout-column>
+                    <wk-layout-column column="2" wkElevation="12" style="height: calc(64px * 3);"></wk-layout-column>
+                    <wk-layout-column column="2" wkElevation="13" style="height: calc(64px * 3);"></wk-layout-column>
+                    <wk-layout-column column="2" wkElevation="14" style="height: calc(64px * 3);"></wk-layout-column>
+                    <wk-layout-column column="2" wkElevation="15" style="height: calc(64px * 3);"></wk-layout-column>
+                    <wk-layout-column column="2" wkElevation="16" style="height: calc(64px * 3);"></wk-layout-column>
+                    <wk-layout-column column="2" wkElevation="17" style="height: calc(64px * 3);"></wk-layout-column>
+                    <wk-layout-column column="2" wkElevation="18" style="height: calc(64px * 3);"></wk-layout-column>
+                    <wk-layout-column column="2" wkElevation="19" style="height: calc(64px * 3);"></wk-layout-column>
+                    <wk-layout-column column="2" wkElevation="20" style="height: calc(64px * 3);"></wk-layout-column>
+                    <wk-layout-column column="2" wkElevation="21" style="height: calc(64px * 3);"></wk-layout-column>
+                    <wk-layout-column column="2" wkElevation="22" style="height: calc(64px * 3);"></wk-layout-column>
+                    <wk-layout-column column="2" wkElevation="23" style="height: calc(64px * 3);"></wk-layout-column>
+                    <wk-layout-column column="2" wkElevation="24" style="height: calc(64px * 3);"></wk-layout-column>
+                </wk-layout-grid>
+            </wk-layout>
         `
     }
 }
---
+++ components/elevation/README.md
@@ -0,0 +1,14 @@
+# wkElevation (Prop)
+
+## Props
+
+Prop Name | Type | Description
+--- | --- | ---
+wkElevation="0" | number | 0-24
+wkElevation="24" | number | 0-24
+
+## Events
+
+Event Name | Params | Description
+--- | --- | ---
+... | ... | ...
