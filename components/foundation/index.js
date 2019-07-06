bd3acda9854e33721db8e68efcbd6143e48bdf9c
Ridho Prasetya <ndiing@users.noreply.github.com>
2019-07-06 15:14:58 +0700

    demo

--- components/foundation/index.js
+++ components/foundation/index.js
@@ -1,6 +1,6 @@
-window.addEventListener("load", event => {
-    document.body.classList.remove("preload")
-})
+// window.addEventListener("load", event => {
+//     document.body.classList.remove("preload")
+// })
 
 // window.addEventListener("contextmenu", event => {
 //     event.preventDefault()
